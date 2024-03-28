9669b6ec8226b66c399dbb68d40d11a68bd38584
Cristian Costea <cristicoss@gmail.com>
2024-03-28 11:51:16 +0100

    added newsletter sumbit to supababse

--- handleNewsletter.js
+++ handleNewsletter.js
@@ -5,8 +5,22 @@ const radio2 = document.getElementById("radio2");
 const permanentForm = document.querySelector(".permanent-form_wrapper");
 const baForm = document.querySelector(".ba-form_wrapper");
 const accept = document.querySelector(".job-alert_checkbox-wrapper");
+const successNews = document.querySelector(".success-news_container");
+const titleNews = document.querySelector(".title-news_container");
+const formContainer = document.querySelector(".popup_form-container");
+
+// const formNews = document.querySelector(".popup_form-container");
+const formNews = document.getElementById("wf-form-Name");
+const emailNews = document.getElementById("email-2");
+const nameNews = document.getElementById("name-2");
+const positionNews = document.getElementById("Fachgebiet-2");
+const experienceNews = document.getElementById("Erfahrung-2");
+const placeNews = document.getElementById("Ort-2");
+
+let employmentType = "";
 
 radio1.addEventListener("click", function () {
+  employmentType = "employment";
   permanentForm.classList.remove("active");
   baForm.classList.remove("active");
   baForm.classList.add("active");
@@ -14,8 +28,73 @@ radio1.addEventListener("click", function () {
 });
 
 radio2.addEventListener("click", function () {
+  employmentType = "brand-ambassador";
+
   baForm.classList.remove("active");
   permanentForm.classList.remove("active");
   permanentForm.classList.add("active");
   accept.classList.remove("active");
+});
+
+/// SUPABASE INIT ///
+const supabaseUrl = "https://zffxtqoaghcuwpnslgth.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpmZnh0cW9hZ2hjdXdwbnNsZ3RoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEzNjM4NzIsImV4cCI6MjAyNjkzOTg3Mn0.PUC3EpGYrLV8ZMKFu428qErvYRV7GtBlf4WfWHJrs-k";
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+formNews.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  console.log(
+    employmentType,
+    emailNews.value,
+    nameNews.value,
+    positionNews.value,
+    experienceNews.value,
+    placeNews.value
+  );
+
+  const insertRow = async () => {
+    if (employmentType === "employment") {
+      const { data, error } = await supabase
+        .from("newsletter-employment")
+        .insert([
+          {
+            email: emailNews.value,
+            name: nameNews.value,
+            position: positionNews.value,
+            experience: experienceNews.value,
+          },
+        ]);
+
+      if (error) {
+        console.error(error);
+      } else {
+        console.log("success");
+        successNews.classList.remove("hidden");
+        titleNews.classList.add("hidden");
+        formContainer.classList.add("hidden");
+      }
+    } else {
+      const { data, error } = await supabase.from("newsletter-BAs").insert([
+        {
+          email: emailNews.value,
+          name: nameNews.value,
+          place: placeNews.value,
+        },
+      ]);
+
+      if (error) {
+        console.error(error);
+      } else {
+        console.log("success");
+        successNews.classList.remove("hidden");
+        titleNews.classList.add("hidden");
+        formContainer.classList.add("hidden");
+      }
+    }
+  };
+
+  insertRow();
 });
