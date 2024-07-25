2e828623350e3b8ee615b7f767114ebf09aef777
cristicoss <cristicoss@gmail.com>
2024-07-25 12:57:11 +0200

    made changing images on studio page work

--- sliderAuto.js
+++ sliderAuto.js
@@ -1,8 +1,9 @@
 "use strict";
 const cardsHolder = document.querySelectorAll(".swipe_cards-1");
-let currIndex = 0;
-// cardsHolder[0].classList.add("active");
+const cardsHolder2 = document.querySelectorAll(".swipe_cards-2");
+let currIndex = 1;
 
+/*
 function showNext() {
   cardsHolder[1].classList.remove("active");
   cardsHolder[2].classList.remove("active");
@@ -15,5 +16,25 @@ function showNext() {
   currIndex = currIndex + 1;
   if (currIndex === 3) currIndex = 0;
 }
+*/
 
-setInterval(showNext, 3000);
+const showImgs = function (cards) {
+  console.log("change");
+  cards.forEach((card) => {
+    card.classList.add("active");
+  });
+  cards[currIndex].classList.remove("active");
+  currIndex++;
+  if (currIndex === 3) currIndex = 0;
+};
+
+setInterval(() => {
+  showImgs(cardsHolder);
+}, 2000);
+
+setInterval(() => {
+  showImgs(cardsHolder2);
+}, 2000);
+// setInterval(showImgs(cardsHolder), 3000);
+
+// setInterval(showNext, 3000);
